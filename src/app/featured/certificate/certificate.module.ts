e0718d57f4f7d0ded2d37c3238f4989d75847013
Fede <fedeverges@gmail.com>
2022-08-07 20:53:46 -0300

    se agrega snackbar

--- src/app/featured/certificate/certificate.module.ts
+++ src/app/featured/certificate/certificate.module.ts
@@ -4,11 +4,12 @@ import { CommonModule } from '@angular/common';
 import { CertificateRoutingModule } from './certificate-routing.module';
 import { NewCertificateComponent } from './new-certificate/new-certificate.component';
 import { SharedModule } from 'src/app/shared/shared.module';
-import { FormBuilder, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { Web3Service } from 'src/app/core/services/web3.service';
 import { MdbFormsModule } from 'mdb-angular-ui-kit/forms';
-import { MdbValidationModule } from 'mdb-angular-ui-kit/validation';
 import { AuthService } from 'src/app/core/services/auth.service';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+
 @NgModule({
   declarations: [
     NewCertificateComponent
@@ -20,6 +21,7 @@ import { AuthService } from 'src/app/core/services/auth.service';
     SharedModule,
     CertificateRoutingModule,
     MdbFormsModule,
+    MatSnackBarModule,
   ],
   providers: [Web3Service, AuthService],
 })
