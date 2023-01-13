ccd74070489e3ceae0b3d0990676eaa841b95f11
Fede <fedeverges@gmail.com>
2023-01-13 11:49:41 -0300

    Mejora en servicio de alerta

--- src/app/core/services/alert.service.ts
+++ src/app/core/services/alert.service.ts
@@ -2,30 +2,35 @@ import { Injectable } from '@angular/core';
 import {
   MatSnackBar,
   MatSnackBarConfig,
-  MatSnackBarRef
+  MatSnackBarHorizontalPosition,
+  MatSnackBarRef,
+  MatSnackBarVerticalPosition
 } from '@angular/material/snack-bar';
 
+const horizontalPosition: MatSnackBarHorizontalPosition = 'start';
+const verticalPosition: MatSnackBarVerticalPosition = 'bottom';
+const duration = 1500;
 @Injectable({
   providedIn: 'root'
 })
 export class AlertService {
+  private DEFAULT_CONFIG: MatSnackBarConfig = {
+    duration: duration,
+    horizontalPosition: horizontalPosition,
+    verticalPosition: verticalPosition
+  };
+
   constructor(private _snackBar: MatSnackBar) {}
 
   showAlertRef(message: string, actions?: string): MatSnackBarRef<any> {
-    return this._snackBar.open(message, actions, {
-      duration: 1000
-    } as MatSnackBarConfig);
+    return this._snackBar.open(message, actions, this.DEFAULT_CONFIG);
   }
 
-  showAlert(
-    message: string,
-    actions?: string,
-    config?: MatSnackBarConfig
-  ): void {
-    this._snackBar.open(message, actions, config);
+  showAlert(message: string, actions?: string, config?: MatSnackBarConfig): void {
+    this._snackBar.open(message, actions, config || this.DEFAULT_CONFIG);
   }
 
   showErrorMessage(message: string) {
-    this._snackBar.open(message, 'Aceptar');
+    this._snackBar.open(message, 'Aceptar', this.DEFAULT_CONFIG);
   }
 }
