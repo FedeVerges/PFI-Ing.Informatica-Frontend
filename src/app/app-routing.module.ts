31b8805b88fc6631bd3e2b160cdd9279a03d6bad
Fede <fedeverges@gmail.com>
2023-06-11 02:04:37 -0300

    ajustes en router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { AuthGuard } from './core/guards/auth.guard';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'student/new', pathMatch: 'full' },
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
   {
     path: 'certificate',
     loadChildren: () =>
